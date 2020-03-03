8a3d18e80eb28dfe9066cc7f0ed9b901e5a084fe
Nathan <naslee2@gmail.com>
2020-03-02 20:15:56 -0800

    added css

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -3,9 +3,11 @@ import styles from '../Intro/Intro.module.css'
 
 const intro = (props) => (
     <div className={styles.Intro}>
-        <p>Burger-It</p>
-        <p>Deliciousness Redefined</p>
-        <button>View Menu</button>
+        <div className={styles.Center}>
+            <p className={styles.CenterTitle}>Burger-It</p>
+            <p className={styles.CenterMotto}>Deliciousness Redefined</p>
+            <button>View Menu</button>
+        </div>
     </div>
 );
 
