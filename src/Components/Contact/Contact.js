eccadabe6ee4e8822dee01243a36434f09e84bb3
Nathan <naslee2@gmail.com>
2020-03-05 21:54:55 -0800

    finalized layout

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,10 +3,12 @@ import styles from '../Contact/Contact.module.css'
 
 const contact = (props) => (
     <div className={styles.Contact}>
-        <p>Free Today? How about tomorrow?</p>
-        <p>We'll be here, 24/7</p>
-        <p>123 Fake Street, Springfield USA</p>
-        <p>234-555-8008</p>
+        <div className={styles.ContactInfo}>
+            <p>Free Today? How about tomorrow?</p>
+            <p>We'll be here, 24/7</p>
+            <p>123 Fake Street, Springfield USA</p>
+            <p>234-555-8008</p>
+        </div>
     </div>
 );
 
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -3,10 +3,12 @@ import styles from '../Menu/Menu.module.css'
 
 const menu = (props) =>(
     <div className={styles.Menu}>
-        <p>Burger-It</p>
-        <p>New to Burger-It? We recommend our original, Burger-It, made from 100% Kobe Beef and smothered with our in-house onion jam</p>
-        <p>Nutrition: 4500 kcal</p>
-        <p>Daily Sodium Intake: 500%</p>
+        <div  className={styles.Menu2}>
+            <p className={styles.Menu2Logo}>Burger-It</p>
+            <p className={styles.Menu2Recommendation}>New to Burger-It? We recommend our original, Burger-It, made from 100% Kobe Beef and smothered with our in-house onion jam</p>
+            <p className={styles.Menu2Nutrition1}>Nutrition: 4500 kcal</p>
+            <p className={styles.Menu2Nutrition2}>Daily Sodium Intake: 500%</p>
+        </div>
     </div>
 );
 
